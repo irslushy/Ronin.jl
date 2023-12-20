c0633a925a3cf408c2bde23ec1358112fbc99723
Isaac Schluesche <irschluesche@gmail.com>
2023-12-20 13:33:24 -0700

    Fixed everything

--- split_data.jl
+++ split_data.jl
@@ -2,37 +2,38 @@ TRAINING_FRAC::Float32 = .72
 VALIDATION_FRAC::Float32 = .08
 TESTING_FRAC:: Float32 = 1 - TRAINING_FRAC - VALIDATION_FRAC
 
-DIR_PATHS::Vector{String} = ["./CFRADIALS/CASES/VORTEX/", "./CFRADIALS/CASES/BAMEX/", "./CFRADIALS/CASES/RITA/", "./CFRADIALS/CASES/HAGUPIT/"]
+DIR_PATHS::Vector{String} = ["/Users/ischluesche/Documents/Grad_School/Research/JMLQC/CFRADIALS/CASES/VORTEX/", 
+"/Users/ischluesche/Documents/Grad_School/Research/JMLQC/CFRADIALS/CASES/BAMEX/",
+"/Users/ischluesche/Documents/Grad_School/Research/JMLQC/CFRADIALS/CASES/RITA/", 
+"/Users/ischluesche/Documents/Grad_School/Research/JMLQC/CFRADIALS/CASES/HAGUPIT/"]
+
+TESTING_PATH::String = "/Users/ischluesche/Documents/Grad_School/Research/JMLQC/CFRADIALS/CASES/TESTING/"
+TRAINING_PATH::String = "/Users/ischluesche/Documents/Grad_School/Research/JMLQC/CFRADIALS/CASES/TRAINING/"
+
+
+###Clean things
+rm(TESTING_PATH, force = true, recursive = true)
+rm(TRAINING_PATH, force = true, recursive = true) 
+
+mkdir(TESTING_PATH)
+mkdir(TRAINING_PATH) 
 
 for path in DIR_PATHS
+
     contents = readdir(path)
     split_location = ceil(Int32, length(contents) * (TRAINING_FRAC + VALIDATION_FRAC))
 
     training_files = contents[1:split_location]
     testing_files = contents[split_location + 1:end]
 
-    printstyled("\nLength of trainign_files: $(length(training_files)) out of $(length(contents))\n", color=:blue)
+    printstyled("\nLength of training files: $(length(training_files)) out of $(length(contents))\n", color=:blue)
     printstyled("Length of testing_files: $(length(testing_files)) out of $(length(contents))\n", color=:blue)
 
-    if isdir(path * "TESTING")
-        for file_name in testing_files
-            symlink(path * file_name, "$(path)TESTING/$(file_name)")
-        end
-    else 
-        mkdir(path * "TESTING")
-        for file_name in testing_files
-            symlink(path * file_name, "$(path)TESTING/$(file_name)")
-        end
+    for file in training_files
+        symlink((path * file), TRAINING_PATH * file)
     end 
 
-    if isdir(path * "TRAINING")
-        for file_name in training_files
-            symlink(path * file_name, "$(path)TRAINING/$(file_name)")
-        end
-    else 
-        mkdir(path * "TRAINING")
-        for file_name in training_files
-            symlink(path * file_name, "$(path)TRAINING/$(file_name)")
-        end
-    end 
+    for file in testing_files
+        symlink((path * file), TESTING_PATH * file)
+    end
 end 
