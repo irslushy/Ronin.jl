d13e4f3054dc7a4cac2b06938274e738244e8247
Isaac Schluesche <irschluesche@gmail.com>
2024-01-23 15:02:08 -0700

    Added directory processing mode

--- QC_scan.jl
+++ QC_scan.jl
@@ -12,7 +12,7 @@ using ScikitLearn
 @sk_import ensemble: RandomForestClassifier
 
 ###Change this to apply quality control to different variables in CFRAD
-VARIALBES_TO_QC::Vector{String} = ["DBZ", "VEL"]
+VARIALBES_TO_QC::Vector{String} = ["ZZ", "VV"]
 ###New variable name in netcdf file will be VARNAME * QC_SUFFIX 
 QC_SUFFIX::String = "_FILTERED"
 
@@ -24,7 +24,7 @@ function parse_commandline()
 
     @add_arg_table s begin
         
-        "CFRad_path"
+        "input_path"
             help = "Path to input CFRadial File"
             required = true
         "model_path"
@@ -37,7 +37,7 @@ function parse_commandline()
             required=true 
         "-m", "--mode"
             help = "Whether to run in directory mode (D), single-scan mode(S), or 
-                    Listening Mode (L). If directory mode, CFRad_path should be a directory
+                    Listening Mode (L). If directory mode, input_path should be a directory
                     containing a variety of scans to apply QC to. If single-scan, this should
                     be a single file. If Listening, path should contain a directory. The script will
                     run until interrupted, processing new files as they are added to the directory"
@@ -56,49 +56,68 @@ function parse_commandline()
     return parse_args(s)
 end
 
+function listen() 
+    throw("LISTEN MODE NOT YET IMPLEMENTEDs") 
+end 
+
 function main()     
     ##Could make this a try-catch block if you really wanted to be 
     ##Extra safe about it 
     parsed_args = parse_commandline() 
 
-    ##Open in append mode so output variables can be written 
-    input_cfrad = NCDataset(parsed_args["CFRad_path"], "a")
-    cfrad_dims = (input_cfrad.dim["range"], input_cfrad.dim["time"])
 
-    ###Will generally NOT return Y, but only (X, indexer)
-    X, Y, indexer = JMLQC_utils.process_single_file(input_cfrad, parsed_args; REMOVE_HIGH_PGG = true, REMOVE_LOW_NCP = true, remove_variable=parsed_args["i"])
+    paths = Vector{String}()
 
-    ##Load saved RF model 
-    ##assume that default SYMBOL for saved model is savedmodel
-    trained_model = BSON.load(parsed_args["model_path"], @__MODULE__)[:currmodel]
-    predictions = ScikitLearn.predict(trained_model, X)
+    if lowercase(parsed_args["mode"]) == "d"
+        paths = parse_directory(parsed_args["input_path"])
+    elseif lowercase(parsed_cargs["mode"]) == "s"
+        paths = [parsed_args["input_path"]]
+    elseif lowercase(parsed_args["mode"]) == "l"
+        listen()
+    else    
+        printstyled("\nINVALID MODE... EXITING\n", color=:red)
+        return 
+    end
 
-    ##QC each variable in VARIALBES_TO_QC
-    for var in VARIALBES_TO_QC
+    for path in paths 
+        ##Open in append mode so output variables can be written 
+        input_cfrad = NCDataset(path, "a")
+        cfrad_dims = (input_cfrad.dim["range"], input_cfrad.dim["time"])
 
-        ##Create new field to reshape QCed field to 
-        NEW_FIELD = missings(Float64, cfrad_dims) 
+        ###Will generally NOT return Y, but only (X, indexer)
+        X, Y, indexer = JMLQC_utils.process_single_file(input_cfrad, parsed_args; REMOVE_HIGH_PGG = true, REMOVE_LOW_NCP = true, remove_variable=parsed_args["i"])
 
-        ##Only modify relevant data based on indexer, everything else should be fill value 
-        QCED_FIELDS = input_cfrad[var][:][indexer]
-        initial_count = count(!iszero, QCED_FIELDS)
-        ##Apply predictions from model 
-        ##If model predicts 1, this indicates a prediction of meteorological data 
-        QCED_FIELDS = map(x -> Bool(predictions[x[1]]) ? x[2] : missing, enumerate(QCED_FIELDS))
-        final_count = count(!ismissing, QCED_FIELDS)
-        
-        ###Need to reconstruct original 
-        NEW_FIELD = NEW_FIELD[:]
-        NEW_FIELD[indexer] = QCED_FIELDS
-        NEW_FIELD = reshape(NEW_FIELD, cfrad_dims)
+        ##Load saved RF model 
+        ##assume that default SYMBOL for saved model is savedmodel
+        trained_model = BSON.load(parsed_args["model_path"], @__MODULE__)[:currmodel]
+        predictions = ScikitLearn.predict(trained_model, X)
 
-        defVar(input_cfrad, var * QC_SUFFIX, NEW_FIELD, ("range", "time"))
+        ##QC each variable in VARIALBES_TO_QC
+        for var in VARIALBES_TO_QC
 
-        println()
-        printstyled("REMOVED $(initial_count - final_count) PRESUMED NON-METEORLOGICAL DATAPOINTS\n", color=:green)
-        println("FINAL COUNT OF DATAPOINTS IN $(var): $(final_count)")
+            ##Create new field to reshape QCed field to 
+            NEW_FIELD = missings(Float64, cfrad_dims) 
+
+            ##Only modify relevant data based on indexer, everything else should be fill value 
+            QCED_FIELDS = input_cfrad[var][:][indexer]
+            initial_count = count(!iszero, QCED_FIELDS)
+            ##Apply predictions from model 
+            ##If model predicts 1, this indicates a prediction of meteorological data 
+            QCED_FIELDS = map(x -> Bool(predictions[x[1]]) ? x[2] : missing, enumerate(QCED_FIELDS))
+            final_count = count(!ismissing, QCED_FIELDS)
+            
+            ###Need to reconstruct original 
+            NEW_FIELD = NEW_FIELD[:]
+            NEW_FIELD[indexer] = QCED_FIELDS
+            NEW_FIELD = reshape(NEW_FIELD, cfrad_dims)
+
+            defVar(input_cfrad, var * QC_SUFFIX, NEW_FIELD, ("range", "time"))
+
+            println()
+            printstyled("REMOVED $(initial_count - final_count) PRESUMED NON-METEORLOGICAL DATAPOINTS\n", color=:green)
+            println("FINAL COUNT OF DATAPOINTS IN $(var): $(final_count)")
+        end 
     end 
-    println("DID IT WORK?")
 end 
 
 main()
