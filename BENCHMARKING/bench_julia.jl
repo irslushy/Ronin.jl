5b037c4b7c6311d9ab216c2cf5f7af2ce21ab306
Isaac Schluesche <irschluesche@gmail.com>
2024-04-11 11:41:40 -0600

    Test benchmarking suite actions

--- BENCHMARKING/bench_julia.jl
+++ BENCHMARKING/bench_julia.jl
@@ -1,15 +1,28 @@
 using RadarQC
 using BenchmarkTools
+using Missings
 
-printstyled("OK\n", color=:red)
-###Hyperparameters from paper: 21 trees, max depth 14
-# currmodel = DecisionTree.RandomForestClassifier(;n_trees=21, max_depth=14)
-# ###Determine the class weights 
+###Will quickly run benchmarks on a series of feature calculations for a cfradial 
 
-# filepath = "./data/raw_Vortex.h5"
-# currset = HDF5.h5open(filepath)
-# x = currset["X"]
-# y = currset["Y"]
+tasks=["ISO(VEL), AHT, STD(VEL), PGG, SQI, DBZ"]
 
-# fit!(currmodel, x[:,:], y[:])
+placeholder_matrix = allowmissing(ones(3,3))
+weight_matrixes = [allowmissing(ones(7,7)), placeholder_matrix, allowmissing(ones(5,5)),
+                    placeholder_matrix, placeholder_matrix, placeholder_matrix]
+
+benchmark_cfrad_locs = "./benchmark_cfrads/"
+
+suite = BenchmarkGroup()
+
+suite["features"] = BenchmarkGroup(["tag1"])
+suite["features"][10] = @benchmarkable calculate_features(benchmark_cfrad_locs, tasks, weight_matrixes, 
+                                                                "garbage.h5", true; verbose=true, 
+                                                                REMOVE_LOW_NCP = true, REMOVE_HIGH_PGG = true, 
+                                                                QC_variable="VT", remove_variable="VEL")
+
+
+tune!(suite) 
+results = run(suite, verbose=true) 
+
+BenchmarkTools.save("output.json", median(results))
 
