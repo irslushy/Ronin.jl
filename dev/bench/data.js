e2fc277d9ffe2680d97c376346c5911d4db92027
github-action-benchmark <github@users.noreply.github.com>
2024-05-01 16:10:55 +0000

    add Julia benchmark result (julia) benchmark result for 83388080992f5666bee5a5524d88dcddde441de7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712874795338,
+  "lastUpdate": 1714579855730,
   "repoUrl": "https://github.com/irslushy/RadarQC.jl",
   "entries": {
     "Julia benchmark result": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 2050790235,
             "unit": "ns",
             "extra": "gctime=126596842\nmemory=2805573960\nallocs=17716686\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "85644232+irslushy@users.noreply.github.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "83388080992f5666bee5a5524d88dcddde441de7",
+          "message": "Merge pull request #12 from cenamiller/main\n\nAdded jl script version for Derecho runs",
+          "timestamp": "2024-05-01T10:04:38-06:00",
+          "tree_id": "98dc1d566eef2349eb05a0d4d0e07f8c7fe68a6e",
+          "url": "https://github.com/irslushy/RadarQC.jl/commit/83388080992f5666bee5a5524d88dcddde441de7"
+        },
+        "date": 1714579854569,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 2064087848,
+            "unit": "ns",
+            "extra": "gctime=126004021\nmemory=2805573960\nallocs=17716686\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
         ]
       }
