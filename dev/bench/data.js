753603e21819f7e28d7179ebc9b59d91b8eeff7b
github-action-benchmark <github@users.noreply.github.com>
2024-05-14 20:51:44 +0000

    add Julia benchmark result (julia) benchmark result for 204059802b6a55f632a76fcdd689286cbe33aa52

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1715719904555,
+  "repoUrl": "https://github.com/irslushy/RadarQC.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "85644232+irslushy@users.noreply.github.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "204059802b6a55f632a76fcdd689286cbe33aa52",
+          "message": "Merge pull request #14 from irslushy/bug_fixes\n\nTesting Suite Development + Benchmark Language Comparisons",
+          "timestamp": "2024-05-14T14:45:35-06:00",
+          "tree_id": "164f1d37dd276341795cb1bff535ba0feac2481e",
+          "url": "https://github.com/irslushy/RadarQC.jl/commit/204059802b6a55f632a76fcdd689286cbe33aa52"
+        },
+        "date": 1715719904006,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 1704521050,
+            "unit": "ns",
+            "extra": "gctime=74000773\nmemory=2252675208\nallocs=19680334\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
