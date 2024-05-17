5658d8db8bc76abc88f547d29c0cc6d57f952427
github-action-benchmark <github@users.noreply.github.com>
2024-05-17 18:29:21 +0000

    add Julia benchmark result (julia) benchmark result for 1ff0f2ad13c5093b64d2c754573cac64311a2911

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715904858421,
+  "lastUpdate": 1715970561802,
   "repoUrl": "https://github.com/irslushy/RadarQC.jl",
   "entries": {
     "Julia benchmark result": [
@@ -60,6 +60,36 @@ window.BENCHMARK_DATA = {
             "value": 1722270593,
             "unit": "ns",
             "extra": "gctime=86567001\nmemory=2252675208\nallocs=19680334\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "1ff0f2ad13c5093b64d2c754573cac64311a2911",
+          "message": "Added model evaluation functionality",
+          "timestamp": "2024-05-17T12:22:22-06:00",
+          "tree_id": "78a2223fe373aa27c7586d3ea03c8ce056abadce",
+          "url": "https://github.com/irslushy/RadarQC.jl/commit/1ff0f2ad13c5093b64d2c754573cac64311a2911"
+        },
+        "date": 1715970561295,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 1676179893,
+            "unit": "ns",
+            "extra": "gctime=81517800\nmemory=2252675208\nallocs=19680334\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
         ]
       }
