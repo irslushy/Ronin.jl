ecf7ad5184595b19490983d842ce5e1d852b6393
github-action-benchmark <github@users.noreply.github.com>
2024-05-24 00:21:56 +0000

    add Julia benchmark result (julia) benchmark result for 8cda13cce94ac49d6f6e5ff945cde9716b9a420f

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1716510116696,
+  "repoUrl": "https://github.com/irslushy/RadarQC.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "8cda13cce94ac49d6f6e5ff945cde9716b9a420f",
+          "message": "Added benchmark setup materials",
+          "timestamp": "2024-05-23T18:14:49-06:00",
+          "tree_id": "4522f77e162ddacd53f2a705b9a29737d9740480",
+          "url": "https://github.com/irslushy/RadarQC.jl/commit/8cda13cce94ac49d6f6e5ff945cde9716b9a420f"
+        },
+        "date": 1716510116086,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 1683179700,
+            "unit": "ns",
+            "extra": "gctime=82492107\nmemory=2252762728\nallocs=19682094\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
