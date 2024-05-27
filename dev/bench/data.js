8e7e2f5b3eabde051b2ebb13113b03c50f21fc95
github-action-benchmark <github@users.noreply.github.com>
2024-05-27 23:32:23 +0000

    add Julia benchmark result (julia) benchmark result for 0421a1a5fd14f9cd8425759208acf4cce0c3dadc

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1716852743346,
+  "repoUrl": "https://github.com/irslushy/RadarQC.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "0421a1a5fd14f9cd8425759208acf4cce0c3dadc",
+          "message": "Untracked tuning notebook",
+          "timestamp": "2024-05-27T17:25:14-06:00",
+          "tree_id": "31e6ecb8be14180d3469b8131aa7319fb0fadd60",
+          "url": "https://github.com/irslushy/RadarQC.jl/commit/0421a1a5fd14f9cd8425759208acf4cce0c3dadc"
+        },
+        "date": 1716852742183,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 1737958976,
+            "unit": "ns",
+            "extra": "gctime=93234337\nmemory=2252762728\nallocs=19682094\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
