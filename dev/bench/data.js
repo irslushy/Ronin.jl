96e723f35951de1e8c96cdadf3836fb57fe38a05
github-action-benchmark <github@users.noreply.github.com>
2024-06-04 20:46:25 +0000

    add Julia benchmark result (julia) benchmark result for 259aa4d306e09fedf9d4208bcc8a584fbabd89a2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717533679185,
+  "lastUpdate": 1717533985336,
   "repoUrl": "https://github.com/irslushy/Ronin.jl",
   "entries": {
     "Julia benchmark result": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 1997144584,
             "unit": "ns",
             "extra": "gctime=352007458\nmemory=2252762728\nallocs=19682094\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "259aa4d306e09fedf9d4208bcc8a584fbabd89a2",
+          "message": "Update README and github workflows",
+          "timestamp": "2024-06-04T14:39:17-06:00",
+          "tree_id": "bcd9fb343160166ea325e101c05ae1935565def8",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/259aa4d306e09fedf9d4208bcc8a584fbabd89a2"
+        },
+        "date": 1717533984701,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 2042511421,
+            "unit": "ns",
+            "extra": "gctime=373783912\nmemory=2252762728\nallocs=19682094\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
         ]
       }
