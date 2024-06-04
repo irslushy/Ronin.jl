77238abee0f832ecac5177083c5edbaa48245832
github-action-benchmark <github@users.noreply.github.com>
2024-06-04 22:27:50 +0000

    add Julia benchmark result (julia) benchmark result for 385e5cb290fcc70541d93854275a33f6b16bc28e

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1717540070695,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "85644232+irslushy@users.noreply.github.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "385e5cb290fcc70541d93854275a33f6b16bc28e",
+          "message": "Update derecho_benchmark.jl",
+          "timestamp": "2024-06-04T16:20:44-06:00",
+          "tree_id": "234d591eb27b7daf30979d2c937022d5cf127966",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/385e5cb290fcc70541d93854275a33f6b16bc28e"
+        },
+        "date": 1717540069484,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 2029433869,
+            "unit": "ns",
+            "extra": "gctime=377017029\nmemory=2252762728\nallocs=19682094\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
