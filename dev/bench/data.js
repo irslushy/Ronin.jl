f87662e24415d356c869ad2e21d11b1ece74ddb3
github-action-benchmark <github@users.noreply.github.com>
2024-06-06 19:40:11 +0000

    add Julia benchmark result (julia) benchmark result for ca80b36974b0a289aeb60b9236860da9e890d614

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1717702811408,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "ca80b36974b0a289aeb60b9236860da9e890d614",
+          "message": "Added documentation section regarding parameter and variable convention reference",
+          "timestamp": "2024-06-06T13:33:02-06:00",
+          "tree_id": "974a2449da80bc48480ca8a776c4d18151a26560",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/ca80b36974b0a289aeb60b9236860da9e890d614"
+        },
+        "date": 1717702810730,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 1780500636,
+            "unit": "ns",
+            "extra": "gctime=123324392\nmemory=2252762760\nallocs=19682095\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
