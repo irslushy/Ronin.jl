62c4c816e12f4c6107966623dd30a756350266cd
github-action-benchmark <github@users.noreply.github.com>
2024-06-07 00:40:20 +0000

    add Julia benchmark result (julia) benchmark result for 52fc707791428d7631e7907d061aa6c1141d8a46

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1717720820962,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "52fc707791428d7631e7907d061aa6c1141d8a46",
+          "message": "Added more graceful error handling and ability to subset columns of an h5 for training",
+          "timestamp": "2024-06-06T18:33:11-06:00",
+          "tree_id": "52afa9bcc5e48a6581d89d3e81bc5d0d3eeca5ff",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/52fc707791428d7631e7907d061aa6c1141d8a46"
+        },
+        "date": 1717720820156,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 2008961643,
+            "unit": "ns",
+            "extra": "gctime=361539111\nmemory=2252762728\nallocs=19682094\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
