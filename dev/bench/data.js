70ef9df75ef1720aee189bb2120dbb0053691ae2
github-action-benchmark <github@users.noreply.github.com>
2024-06-10 19:33:29 +0000

    add Julia benchmark result (julia) benchmark result for 3398d317c6d964550fb86e99ad604beaa471e617

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718047792384,
+  "lastUpdate": 1718048009443,
   "repoUrl": "https://github.com/irslushy/Ronin.jl",
   "entries": {
     "Julia benchmark result": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 1770115600,
             "unit": "ns",
             "extra": "gctime=126363988\nmemory=2252762728\nallocs=19682094\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "3398d317c6d964550fb86e99ad604beaa471e617",
+          "message": "Documentation formatting update",
+          "timestamp": "2024-06-10T13:26:28-06:00",
+          "tree_id": "d50de2a27f2a77ac198e2ce3457c001b632f85b4",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/3398d317c6d964550fb86e99ad604beaa471e617"
+        },
+        "date": 1718048008953,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 2002098046,
+            "unit": "ns",
+            "extra": "gctime=345380174\nmemory=2252762760\nallocs=19682095\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
         ]
       }
