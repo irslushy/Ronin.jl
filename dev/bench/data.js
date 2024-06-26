8439f3e7d2b73ff709072bb1c0812b6e2c1db191
github-action-benchmark <github@users.noreply.github.com>
2024-06-26 17:59:44 +0000

    add Julia benchmark result (julia) benchmark result for 35ac8b4570d4db349c2bd07a11b57d041dd920aa

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1719424784320,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "35ac8b4570d4db349c2bd07a11b57d041dd920aa",
+          "message": "Added all-parameter NOAA model",
+          "timestamp": "2024-06-26T11:52:05-06:00",
+          "tree_id": "c2d0de0ec693ba47b858d91414ba62675e3cf11c",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/35ac8b4570d4db349c2bd07a11b57d041dd920aa"
+        },
+        "date": 1719424783126,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 2041225810,
+            "unit": "ns",
+            "extra": "gctime=392940779\nmemory=2252762760\nallocs=19682095\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
