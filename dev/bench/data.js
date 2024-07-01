66e745c001a5bbc65435bc8d9d03b66b83bff830
github-action-benchmark <github@users.noreply.github.com>
2024-07-01 23:33:08 +0000

    add Julia benchmark result (julia) benchmark result for b8885165d342a06d37b2602ac586dc129a95e800

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1719876788554,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "b8885165d342a06d37b2602ac586dc129a95e800",
+          "message": "Updated",
+          "timestamp": "2024-07-01T17:25:46-06:00",
+          "tree_id": "7a85a9196a12b45c040ab3d8a225441b90180786",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/b8885165d342a06d37b2602ac586dc129a95e800"
+        },
+        "date": 1719876787537,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 2082984319,
+            "unit": "ns",
+            "extra": "gctime=387857249\nmemory=2252762728\nallocs=19682094\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
