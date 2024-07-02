c6cd235d02c3c0e73259aba6439dfd9db1081112
github-action-benchmark <github@users.noreply.github.com>
2024-07-02 22:01:54 +0000

    add Julia benchmark result (julia) benchmark result for b07c4f00235ff2148e1e3e2d55e3b50afccfde85

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1719957714247,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "85644232+irslushy@users.noreply.github.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b07c4f00235ff2148e1e3e2d55e3b50afccfde85",
+          "message": "Merge pull request #20 from irslushy/develop-threading\n\nAdded QC mask outputting to QC_scan",
+          "timestamp": "2024-07-02T15:54:39-06:00",
+          "tree_id": "6ebc667e6825733710c6db6caf8e57936ab015e9",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/b07c4f00235ff2148e1e3e2d55e3b50afccfde85"
+        },
+        "date": 1719957712987,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 875484495,
+            "unit": "ns",
+            "extra": "gctime=13463810.5\nmemory=555150920\nallocs=7063838\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
