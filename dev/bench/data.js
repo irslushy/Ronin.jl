3b0a98153bb1d4a56f7e8af4bdaff789da7d7dbf
github-action-benchmark <github@users.noreply.github.com>
2024-07-08 22:37:52 +0000

    add Julia benchmark result (julia) benchmark result for b14a2794089309d427f900d2a30f870ba4112c29

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1720478272224,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "b14a2794089309d427f900d2a30f870ba4112c29",
+          "message": "Added flowchart to documentation",
+          "timestamp": "2024-07-08T16:30:44-06:00",
+          "tree_id": "6aa0d56fe919f6601d0fdcc26fce464521936a54",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/b14a2794089309d427f900d2a30f870ba4112c29"
+        },
+        "date": 1720478271252,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 840720154,
+            "unit": "ns",
+            "extra": "gctime=13983649.5\nmemory=555150920\nallocs=7063838\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
