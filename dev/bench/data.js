f3f002819359a3078fb18a55870dc6fdfa2a08ff
github-action-benchmark <github@users.noreply.github.com>
2024-07-11 17:56:16 +0000

    add Julia benchmark result (julia) benchmark result for 88a8db1e6fb7064a87ba6f3868e49f520d74002f

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1720720576767,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "85644232+irslushy@users.noreply.github.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "88a8db1e6fb7064a87ba6f3868e49f520d74002f",
+          "message": "Merge pull request #21 from irslushy/develop-threading\n\nMore aggressive test suite + Error evaluation capabilities",
+          "timestamp": "2024-07-11T11:49:18-06:00",
+          "tree_id": "afac423abcf635672599b74e68d3fc3b1195ead2",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/88a8db1e6fb7064a87ba6f3868e49f520d74002f"
+        },
+        "date": 1720720576006,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 883275562,
+            "unit": "ns",
+            "extra": "gctime=13521492.5\nmemory=555150920\nallocs=7063838\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
