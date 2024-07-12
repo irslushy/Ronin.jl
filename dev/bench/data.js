3bc0a10aeaa8f93eeaca7b8d48394cd7310fba71
github-action-benchmark <github@users.noreply.github.com>
2024-07-12 16:27:45 +0000

    add Julia benchmark result (julia) benchmark result for 211b541434886baa95deb3af99391264004a43c2

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1720801665482,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "85644232+irslushy@users.noreply.github.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "211b541434886baa95deb3af99391264004a43c2",
+          "message": "Merge pull request #22 from irslushy/develop-threading\n\nAdded capability to output model error characteristics to file",
+          "timestamp": "2024-07-12T10:20:31-06:00",
+          "tree_id": "c0cc004b7214c1dfd8ed626e2a2849ab7b89c2c0",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/211b541434886baa95deb3af99391264004a43c2"
+        },
+        "date": 1720801664423,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 866810190,
+            "unit": "ns",
+            "extra": "gctime=14455805\nmemory=555150920\nallocs=7063838\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
