bcb03f117a2aeea483ae8a9616640a94dc390b74
github-action-benchmark <github@users.noreply.github.com>
2024-08-20 19:08:50 +0000

    add Julia benchmark result (julia) benchmark result for 5b26df1dcd8f0e213c223afb4162e529993c5d69

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721758375866,
+  "lastUpdate": 1724180930485,
   "repoUrl": "https://github.com/irslushy/Ronin.jl",
   "entries": {
     "Julia benchmark result": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 866068768,
             "unit": "ns",
             "extra": "gctime=13737197.5\nmemory=555150920\nallocs=7063838\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "5b26df1dcd8f0e213c223afb4162e529993c5d69",
+          "message": "Remove extraneous print statements",
+          "timestamp": "2024-08-20T13:01:29-06:00",
+          "tree_id": "b78f64483e6593a28969758f2b36a7591aa3ae8a",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/5b26df1dcd8f0e213c223afb4162e529993c5d69"
+        },
+        "date": 1724180929194,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 862054799,
+            "unit": "ns",
+            "extra": "gctime=14162619.5\nmemory=555150920\nallocs=7063838\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
         ]
       }
