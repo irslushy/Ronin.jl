2c09fd976c8afe24451e32c2c0105fc71b1dab9f
github-action-benchmark <github@users.noreply.github.com>
2024-09-23 19:36:06 +0000

    add Julia benchmark result (julia) benchmark result for 8341916a1ce7ec788b62f3dd807b04fecc62f58a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726082190015,
+  "lastUpdate": 1727120166950,
   "repoUrl": "https://github.com/irslushy/Ronin.jl",
   "entries": {
     "Julia benchmark result": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 871552419.5,
             "unit": "ns",
             "extra": "gctime=14180159.5\nmemory=555151720\nallocs=7063858\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "8341916a1ce7ec788b62f3dd807b04fecc62f58a",
+          "message": "Added capability to output model prediction mask",
+          "timestamp": "2024-09-23T13:29:10-06:00",
+          "tree_id": "bf8d96a846bfe45d61de159c00a694e7856bb385",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/8341916a1ce7ec788b62f3dd807b04fecc62f58a"
+        },
+        "date": 1727120166182,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 853066915,
+            "unit": "ns",
+            "extra": "gctime=13457826.5\nmemory=555151720\nallocs=7063858\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
         ]
       }
