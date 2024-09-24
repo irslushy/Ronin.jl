e86dcf429a2c5972892a24ca4024e2d51b611dd0
github-action-benchmark <github@users.noreply.github.com>
2024-09-24 20:39:55 +0000

    add Julia benchmark result (julia) benchmark result for c0909a338eafa8c3e3383ef02bde25bfb985f4d9

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1727210395500,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "c0909a338eafa8c3e3383ef02bde25bfb985f4d9",
+          "message": "Updated comments and added capability for returning probabilities from composite_prediction",
+          "timestamp": "2024-09-24T14:33:02-06:00",
+          "tree_id": "7cad4fa96cbc7663affd07fe6c9c6b2c8f85d327",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/c0909a338eafa8c3e3383ef02bde25bfb985f4d9"
+        },
+        "date": 1727210394438,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 886702346.5,
+            "unit": "ns",
+            "extra": "gctime=13720884\nmemory=555151720\nallocs=7063858\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
