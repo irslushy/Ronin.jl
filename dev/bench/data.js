0a447ae98bb581ba1755ade0dfc58cd949363132
github-action-benchmark <github@users.noreply.github.com>
2024-09-24 22:03:26 +0000

    add Julia benchmark result (julia) benchmark result for 6fc3d217f705b0876eb2179ad6884f9da2f80b91

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1727215406444,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "6fc3d217f705b0876eb2179ad6884f9da2f80b91",
+          "message": "Added multipass uncertainty functionality",
+          "timestamp": "2024-09-24T15:56:39-06:00",
+          "tree_id": "409920af50b0195c605d98f9e14e7e290a6b751e",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/6fc3d217f705b0876eb2179ad6884f9da2f80b91"
+        },
+        "date": 1727215405211,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 848984070.5,
+            "unit": "ns",
+            "extra": "gctime=13588224.5\nmemory=555151720\nallocs=7063858\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
