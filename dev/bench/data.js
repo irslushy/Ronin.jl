89f2dab4c496f8ab3a76c7b3d9049ea71a8bcf5d
github-action-benchmark <github@users.noreply.github.com>
2024-10-21 18:01:16 +0000

    add Julia benchmark result (julia) benchmark result for ffe455d77a2a63619744114ef2832898009ede4c

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1729533676841,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "ffe455d77a2a63619744114ef2832898009ede4c",
+          "message": "Fixed typing bug",
+          "timestamp": "2024-10-21T11:53:02-06:00",
+          "tree_id": "00eabbb74a5ae0023cb0ac9a579a874fb5faa6db",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/ffe455d77a2a63619744114ef2832898009ede4c"
+        },
+        "date": 1729533675464,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 848364116.5,
+            "unit": "ns",
+            "extra": "gctime=13305166.5\nmemory=555153976\nallocs=7063902\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
