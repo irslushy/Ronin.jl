d4ef1feeec98a802ba438e5c3149a5152b5c28b5
github-action-benchmark <github@users.noreply.github.com>
2024-10-22 21:13:17 +0000

    add Julia benchmark result (julia) benchmark result for c0034f10d65c1822109d26460510e999d03d05a6

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1729631597819,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "85644232+irslushy@users.noreply.github.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c0034f10d65c1822109d26460510e999d03d05a6",
+          "message": "Update README.md\n\nCorrected NOAA data conventions",
+          "timestamp": "2024-10-22T15:05:41-06:00",
+          "tree_id": "ae17b8683fa66c79bea40f8b211231eb3c32657c",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/c0034f10d65c1822109d26460510e999d03d05a6"
+        },
+        "date": 1729631597267,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 853840106.5,
+            "unit": "ns",
+            "extra": "gctime=14806022.5\nmemory=555859208\nallocs=7066749\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
