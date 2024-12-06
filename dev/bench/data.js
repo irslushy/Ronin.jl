41688aae0a78cd518d08b560a629f97d0f27180c
github-action-benchmark <github@users.noreply.github.com>
2024-12-06 19:55:50 +0000

    add Julia benchmark result (julia) benchmark result for ea9a3011fc235e84492660c8ad3632c8fda9588c

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1733514949961,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "85644232+irslushy@users.noreply.github.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ea9a3011fc235e84492660c8ad3632c8fda9588c",
+          "message": "Merge pull request #27 from irslushy/develop_multi_pass\n\nMultipass bug fixes and documentation improvements",
+          "timestamp": "2024-12-06T12:47:49-07:00",
+          "tree_id": "0c69b5b388128a0ac4ff4f46b14e2ec1599990fe",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/ea9a3011fc235e84492660c8ad3632c8fda9588c"
+        },
+        "date": 1733514949134,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 865851829.5,
+            "unit": "ns",
+            "extra": "gctime=13360025.5\nmemory=555859160\nallocs=7066748\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
