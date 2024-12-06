89e19f1e70e6857133ef4e18452b24d19243cf1c
github-action-benchmark <github@users.noreply.github.com>
2024-12-06 19:58:39 +0000

    add Julia benchmark result (julia) benchmark result for bca249be9165ce3fbff076b48eaf4322507e3dbe

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1733515119352,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "bca249be9165ce3fbff076b48eaf4322507e3dbe",
+          "message": "Remove deprecated example notebooks",
+          "timestamp": "2024-12-06T12:51:01-07:00",
+          "tree_id": "6c82825d5be2167f32359c29b1fbd5e047e93583",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/bca249be9165ce3fbff076b48eaf4322507e3dbe"
+        },
+        "date": 1733515116722,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 848198256.5,
+            "unit": "ns",
+            "extra": "gctime=10883132.5\nmemory=555859160\nallocs=7066748\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
