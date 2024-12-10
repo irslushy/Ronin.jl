ea596f150bd8b388b1caeadea8c6093378615a1b
github-action-benchmark <github@users.noreply.github.com>
2024-12-10 18:51:16 +0000

    add Julia benchmark result (julia) benchmark result for 6770c8e7b66cb2de89dfced6c054dc5369e7ee53

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1733856676836,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "6770c8e7b66cb2de89dfced6c054dc5369e7ee53",
+          "message": "Added constants file for code clarity",
+          "timestamp": "2024-12-10T11:43:36-07:00",
+          "tree_id": "bc1fa1161a8da825eef623a016755504eaf7bfa7",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/6770c8e7b66cb2de89dfced6c054dc5369e7ee53"
+        },
+        "date": 1733856675750,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 846717544.5,
+            "unit": "ns",
+            "extra": "gctime=10173709.5\nmemory=555859160\nallocs=7066748\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
