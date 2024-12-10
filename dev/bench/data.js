df3ae07c4180938ff4cdd0fe87c33752aaeb2664
github-action-benchmark <github@users.noreply.github.com>
2024-12-10 22:48:41 +0000

    add Julia benchmark result (julia) benchmark result for c6778f61c3653371db7a01acad1741b56161ae00

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733870905037,
+  "lastUpdate": 1733870920979,
   "repoUrl": "https://github.com/irslushy/Ronin.jl",
   "entries": {
     "Julia benchmark result": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 851775835,
             "unit": "ns",
             "extra": "gctime=9842932\nmemory=555859160\nallocs=7066748\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "c6778f61c3653371db7a01acad1741b56161ae00",
+          "message": "Added configuration for basic research-quality model configuration",
+          "timestamp": "2024-12-10T15:41:15-07:00",
+          "tree_id": "946101b85e14dbb030682789db18ca969b2e4727",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/c6778f61c3653371db7a01acad1741b56161ae00"
+        },
+        "date": 1733870920468,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 852411770,
+            "unit": "ns",
+            "extra": "gctime=10982477\nmemory=555859208\nallocs=7066749\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
         ]
       }
