a393cbc552a4fe56df7acccaf2ed43777ea8ab26
github-action-benchmark <github@users.noreply.github.com>
2024-12-12 17:23:33 +0000

    add Julia benchmark result (julia) benchmark result for 20ac6c18195b110e105b368b11362c7678f360c7

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1734024213675,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "20ac6c18195b110e105b368b11362c7678f360c7",
+          "message": "Added aggressive NMD removal model configuration",
+          "timestamp": "2024-12-12T10:16:09-07:00",
+          "tree_id": "15638f98dd22ce01c9eee84f2ea43de076a822de",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/20ac6c18195b110e105b368b11362c7678f360c7"
+        },
+        "date": 1734024213247,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 847998616,
+            "unit": "ns",
+            "extra": "gctime=10174691\nmemory=555859160\nallocs=7066748\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
