5187487a957183032b0a5953f79048a585aeeb99
github-action-benchmark <github@users.noreply.github.com>
2025-02-05 17:33:30 +0000

    add Julia benchmark result (julia) benchmark result for 72285ab7709979f2d3ab82a53b021661d46f0887

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1734639617538,
+  "lastUpdate": 1738776810367,
   "repoUrl": "https://github.com/irslushy/Ronin.jl",
   "entries": {
     "Julia benchmark result": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 877089639,
             "unit": "ns",
             "extra": "gctime=15893953\nmemory=555965128\nallocs=7069261\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "irschluesche@gmail.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "distinct": true,
+          "id": "72285ab7709979f2d3ab82a53b021661d46f0887",
+          "message": "Updates",
+          "timestamp": "2025-02-05T10:25:38-07:00",
+          "tree_id": "516bb3be5d8fd0ad17a8580857fd97e6677c7684",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/72285ab7709979f2d3ab82a53b021661d46f0887"
+        },
+        "date": 1738776809396,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 871101009.5,
+            "unit": "ns",
+            "extra": "gctime=16164296.5\nmemory=555965080\nallocs=7069258\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
         ]
       }
