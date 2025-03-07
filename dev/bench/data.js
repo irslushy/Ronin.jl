8e96a05492ecb7b657b57655d2b6020f0717b9ab
github-action-benchmark <github@users.noreply.github.com>
2025-03-07 21:12:32 +0000

    add Julia benchmark result (julia) benchmark result for abe3b463a0b608e2f3b912893a9273cebc1938ec

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1741381952409,
+  "repoUrl": "https://github.com/irslushy/Ronin.jl",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "85644232+irslushy@users.noreply.github.com",
+            "name": "Isaac Schluesche",
+            "username": "irslushy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "abe3b463a0b608e2f3b912893a9273cebc1938ec",
+          "message": "Merge pull request #29 from irslushy/backend_refactor\n\nFixed bugs in the gate misclassification characterization code and made some backend performance improvements.",
+          "timestamp": "2025-03-07T14:04:45-07:00",
+          "tree_id": "fa159e22654f85e65f83cc648f162be7ecf45c53",
+          "url": "https://github.com/irslushy/Ronin.jl/commit/abe3b463a0b608e2f3b912893a9273cebc1938ec"
+        },
+        "date": 1741381950921,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 857415697.5,
+            "unit": "ns",
+            "extra": "gctime=15033876.5\nmemory=555965128\nallocs=7069259\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
